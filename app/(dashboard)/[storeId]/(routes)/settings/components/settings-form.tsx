7745f69e55d8cd5a20e063d09270409c7aa9e5ca
Pranto mollick <prantomollick01@gmail.com>
2023-10-29 19:57:39 +0600

    zod add into store settings form validation

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import React, { useState } from "react";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1)
+});
+
+type SettingsFormValue = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+  });
+
+  const onSubmit = async (data: SettingsFormValue) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button
+          variant="destructive"
+          size="sm"
+          onClick={() => setIsOpen(true)}
+          disabled={isLoading}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} className="ml-auto" type="submit">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default SettingsForm;
---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+  } catch (error) {}
+}
