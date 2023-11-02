3062f404eed23171dd2af9ad5f3ec1c722cb73d1
Pranto Mollick <prantocse01@gmail.com>
2023-11-02 12:27:33 +0600

    billboard clients and billboard image upload component create into the app router

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -19,7 +19,9 @@ const BillboardClient = () => {
           title="Billboards (0)"
           description="Manage Billboards for your store"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <Plus className="mr-2 h-4 w-4" />
         </Button>
       </div>
---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+  disable?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disable,
+  onChange,
+  onRemove,
+  value
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) return null;
+  return <div>ImageUpload</div>;
+};
+
+export default ImageUpload;
