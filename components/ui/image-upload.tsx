b47afa3b6ec15028661ee4aba58f615633a767ed
Pranto Mollick <prantocse01@gmail.com>
2023-11-02 13:26:42 +0600

    cloudinery cldupload widget configuration in image-upload client rendering component function

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import { Button } from "@/components/ui/button";
+import { ImagePlus, Trash } from "lucide-react";
 
 interface ImageUploadProps {
   disable?: boolean;
@@ -26,7 +29,49 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   };
 
   if (!isMounted) return null;
-  return <div>ImageUpload</div>;
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <CldUploadWidget uploadPreset="<Upload Preset>">
+        {({ open }) => {
+          function handleOnClick(e: React.MouseEvent<HTMLElement>) {
+            e.preventDefault();
+            open();
+          }
+          return (
+            <Button
+              type="button"
+              disabled={disable}
+              variant="secondary"
+              onClick={handleOnClick}
+            >
+              <ImagePlus className="h-4 w-4 mr-2" />
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
 };
 
 export default ImageUpload;
