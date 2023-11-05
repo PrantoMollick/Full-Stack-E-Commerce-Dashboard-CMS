1c934fdbe0445c5dcfffc40c8c88d081ac95a832
Pranto Mollick <prantocse01@gmail.com>
2023-11-05 13:32:27 +0600

    working on Api list in  the billboard section

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { Separator } from "@radix-ui/react-separator";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import ApiList from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -33,6 +34,9 @@ const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList />
     </>
   );
 };
